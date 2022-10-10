73c6a6f29c85a3d129be100c0d5e3e081b3a8f03
Asad ali <asadwd666@gmail.com>
2022-10-10 02:38:23 -0700

    my second commit

--- app.js
+++ app.js
@@ -4,23 +4,20 @@ function myFun(){
    
    let centiMeter=2.54 ;
    let result=document.querySelector('.heightresult').innerHTML=height*centiMeter  ;
-   
+   let heightinMeter=result/100;
+
    ///////////////////////////////////////////////////////////// weight conversion
    let weight=document.getElementById('weight').value;
    let pound=2.2046;
    let weightresult=document.getElementById('weightresult').innerText=weight/pound ;
-let heightResult=result;
-let weightResult=weightresult*weightresult;
-if(heightResult && weightResult){
-console.log('BMI :',heightResult/weightResult)
+// let heightResult=result;
+let heightsquare=heightinMeter*heightinMeter;
+if(heightsquare && weightresult){
+console.log('BMI :',weightresult/heightsquare)
 }
 
 }
 
 
-// let result=document.querySelector('.heightresult').value;
 
-// let weightresult=document.getElementById('weightresult').value;
-// let BMI=weightresult/Math.pow(result);
-// console.log(BMI)
 
